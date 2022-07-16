2ff587dad085110c31bd46d1b6ef1ec04722c765
Vincze Gábor <vinczegabor1981@gmail.com>
2022-07-16 11:00:36 +0200

    create 2 tests

--- src/test/java/net/gvincze/javatomcatsample/JavaTomcatSampleApplicationTests.java
+++ src/test/java/net/gvincze/javatomcatsample/JavaTomcatSampleApplicationTests.java
@@ -3,11 +3,21 @@ package net.gvincze.javatomcatsample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class JavaTomcatSampleApplicationTests {
 
     @Test
-    void contextLoads() {
+    void trueTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void falseTest()
+    {
+        assertFalse(false);
     }
 
 }
